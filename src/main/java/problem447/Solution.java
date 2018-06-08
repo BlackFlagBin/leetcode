cb458f54683efed44fa878e1c70891f1462b78fc
任志彬 <renzb@51weker.cn>
2018-06-08 11:04:44 +0800

    447. 回旋镖的数量

--- src/main/java/problem447/Solution.java
+++ src/main/java/problem447/Solution.java
@@ -19,8 +19,51 @@ package problem447;
 两个回旋镖为 [[1,0],[0,0],[2,0]] 和 [[1,0],[2,0],[0,0]]
  */
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.numberOfBoomerangs(new int[][]{{0, 0}, {1, 0}, {2, 0}});
+        System.out.println(result);
+    }
+
     public int numberOfBoomerangs(int[][] points) {
-        return 0;
+        int result = 0;
+
+        for (int i = 0; i < points.length; i++) {
+
+            //key:点i距离的平方,value:相同距离的点的个数
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            for (int j = 0; j < points.length; j++) {
+                if (i != j) {
+                    Integer value = map.get(distance(points[i], points[j]));
+                    if (value == null) {
+                        map.put(distance(points[i], points[j]), 1);
+                    } else {
+                        map.put(distance(points[i], points[j]), ++value);
+                    }
+                }
+            }
+
+            Set<Integer> keySet = map.keySet();
+            Iterator<Integer> iterator = keySet.iterator();
+            while (iterator.hasNext()) {
+                Integer value = map.get(iterator.next());
+                if (value >= 2) {
+                    result += value * (value - 1);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    private int distance(int[] first, int[] second) {
+        return (first[0] - second[0]) * (first[0] - second[0]) + (first[1] - second[1]) * (first[1] - second[1]);
     }
 }

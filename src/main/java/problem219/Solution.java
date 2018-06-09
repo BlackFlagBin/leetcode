5b5ea6caa1f0dc3684e80f8ac33c6073209ef46a
任志彬 <renzb@51weker.cn>
2018-06-09 09:55:06 +0800

    219. 存在重复元素 II

---
+++ src/main/java/problem219/Solution.java
@@ -0,0 +1,40 @@
+package problem219;
+
+/*
+219. 存在重复元素 II
+
+给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的绝对值最大为 k。
+
+示例 1:
+
+输入: nums = [1,2,3,1], k = 3
+输出: true
+示例 2:
+
+输入: nums = [1,0,1,1], k = 1
+输出: true
+示例 3:
+
+输入: nums = [1,2,3,1,2,3], k = 2
+输出: false
+ */
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        //滑动窗口,保持set中的元素个数为k
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+            if (set.size() == k + 1) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}

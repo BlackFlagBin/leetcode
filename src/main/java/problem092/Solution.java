7d3dea2c44b918747a649a4b40d46e6fe7b10026
任志彬 <renzb@51weker.cn>
2018-06-10 20:33:19 +0800

    92. 反转链表 II

--- src/main/java/problem092/Solution.java
+++ src/main/java/problem092/Solution.java
@@ -1,33 +1,80 @@
 package problem092;
 
 /**
- *92. 反转链表 II
- *
+ * 92. 反转链表 II
+ * <p>
  * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
-
- 说明:
- 1 ≤ m ≤ n ≤ 链表长度。
-
- 示例:
-
- 输入: 1->2->3->4->5->NULL, m = 2, n = 4
- 输出: 1->4->3->2->5->NULL
-
+ * <p>
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ * <p>
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        return null;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        solution.reverseBetween(head, 2, 4);
+        ListNode next = head.next;
+        while (next != null) {
+            System.out.println(next.val);
+            next = next.next;
+        }
     }
 
-    public class ListNode {
-        int val;
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == n) {
+            return head;
+        }
+
+        ListNode pre = null;
+        ListNode cur = head;
+        for (int i = 1; i < m; i++) {
+            pre = cur;
+            cur = cur.next;
+        }
+
+        ListNode first = pre;
+        ListNode end = cur;
+        pre = cur;
+        cur = cur.next;
+
+        for (int i = m; i < n; i++) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        if (first != null) {
+            first.next = pre;
+        } else {
+            head=pre;
+        }
+        end.next = cur;
+
+
+        return head;
+    }
+
+    public static class ListNode {
+        int      val;
         ListNode next;
+
         ListNode(int x) { val = x; }
     }
 }

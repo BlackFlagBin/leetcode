4811a6fb33be512fd48ab7fc364bd8526340d978
任志彬 <renzb@51weker.cn>
2018-06-13 11:40:59 +0800

    二叉树的前中后序遍历的迭代解法

--- src/main/java/problem145/Solution.java
+++ src/main/java/problem145/Solution.java
@@ -2,6 +2,7 @@ package problem145;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 145. 二叉树的后序遍历
@@ -21,7 +22,7 @@ import java.util.List;
  * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 class Solution {
-
+    //递归解法
     public List<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> partList = new ArrayList<>();
         if (root != null) {
@@ -31,6 +32,42 @@ class Solution {
         }
 
         return partList;
+    }
+
+    //迭代解法
+    public List<Integer> postorderTraversalIterate(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        //利用指令栈进行迭代
+        Stack<Command> commandStack = new Stack<>();
+        commandStack.push(new Command("go", root));
+        while (commandStack.size()!=0) {
+            Command topCommand = commandStack.pop();
+            if (topCommand.command.equals("visit")) {
+                list.add(topCommand.node.val);
+            } else {
+                commandStack.push(new Command("visit", topCommand.node));
+                if (topCommand.node.right != null) {
+                    commandStack.push(new Command("go", topCommand.node.right));
+                }
+                if (topCommand.node.left != null) {
+                    commandStack.push(new Command("go", topCommand.node.left));
+                }
+            }
+        }
+        return list;
+    }
+
+    private class Command {
+        String command;
+        TreeNode node;
+
+        public Command(String command, TreeNode node) {
+            this.command = command;
+            this.node = node;
+        }
     }
 
     public class TreeNode {

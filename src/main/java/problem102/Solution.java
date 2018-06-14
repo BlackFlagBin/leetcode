db5bba0f727abd3d18a304c4e311d8a06b1d7d6d
任志彬 <renzb@51weker.cn>
2018-06-14 09:50:47 +0800

    二叉树的前中后序遍历的迭代解法

---
+++ src/main/java/problem102/Solution.java
@@ -0,0 +1,45 @@
+package problem102;
+
+import java.util.List;
+
+/**
+ * 102. 二叉树的层次遍历
+ *
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+
+ 例如:
+ 给定二叉树: [3,9,20,null,null,15,7],
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ 返回其层次遍历结果：
+
+ [
+ [3],
+ [9,20],
+ [15,7]
+ ]
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}

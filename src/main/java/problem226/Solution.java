4c5497e79371bc6e68ae7de24ee33f6905c5ca1b
任志彬 <renzb@51weker.cn>
2018-06-21 14:09:47 +0800

    112. 路径总和

--- src/main/java/problem226/Solution.java
+++ src/main/java/problem226/Solution.java
@@ -36,14 +36,16 @@ package problem226;
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-
-        if (root != null) {
-            TreeNode temp = root.left;
-            root.left = root.right;
-            root.right = temp;
-            invertTree(root.left);
-            invertTree(root.right);
+        if (root == null) {
+            return null;
         }
+
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
 
         return root;
     }

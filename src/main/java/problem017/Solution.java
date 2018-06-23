9fe858e780ea940ef99be6040858fa90ea52cccb
任志彬 <renzb@51weker.cn>
2018-06-23 11:25:12 +0800

    17. 电话号码的字母组合

--- src/main/java/problem017/Solution.java
+++ src/main/java/problem017/Solution.java
@@ -1,7 +1,10 @@
 package problem017;
 
+import java.util.ArrayList;
 import java.util.List;
 /*
+17. 电话号码的字母组合
+
 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
 
 给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
@@ -17,7 +20,28 @@ import java.util.List;
  */
 
 class Solution {
+    private String[]     letterMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    private List<String> result    = new ArrayList();
+
     public List<String> letterCombinations(String digits) {
-        return null;
+        if (digits.isEmpty()) {
+            return result;
+        }
+        findCombination(digits, 0, "");
+        return result;
+    }
+
+    private void findCombination(String digits, int index, String lastStr) {
+        if (index == digits.length()) {
+            result.add(lastStr);
+            return;
+        }
+
+        char c = digits.charAt(index);
+        String s = letterMap[c - '0'];
+        for (int i = 0; i < s.length(); i++) {
+            findCombination(digits, index + 1, lastStr + s.charAt(i));
+        }
+
     }
 }
